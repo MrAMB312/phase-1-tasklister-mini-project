fab16d286555d3bff0bd13eefe4b097a99525fda
MrAMB312 <a.blomarz@gmail.com>
2024-11-09 15:06:47 -0600

    complete phase 1 tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // code here
+  // begin by declaring variables
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+  // create event listener with event.preventDefault()
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // get the value from the input field
+    const newTaskDescription = document.getElementById("new-task-description").value;
+
+    // create a new list item (li) element to hold the task
+    const newTask = document.createElement("li");
+    newTask.textContent = newTaskDescription;
+
+    // add the new task to the task list
+    taskList.appendChild(newTask);
+
+    // clear the input field after submitting
+    document.getElementById("new-task-description").value = "";
+
+    // add a delete button next to each task
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", function() {
+      newTask.remove();
+    })
+    newTask.appendChild(deleteButton);
+  });
 });
